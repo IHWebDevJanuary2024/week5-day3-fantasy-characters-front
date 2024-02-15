65d3631b3ceed5e453eadefc1f1e191bf2980858
Marshall-Bits <kz_@hotmail.es>
2024-02-15 09:43:26 +0100

    add equipment

--- src/pages/EquipmentPage.jsx
+++ src/pages/EquipmentPage.jsx
@@ -2,6 +2,11 @@ function EquipmentPage( ){
     return(
         <>
         <h3>This is the equipment page</h3>
+            <ol>
+                <li>Shield</li>
+                <li>Magic keyboard</li>
+                <li>Sword</li>
+            </ol>
         </>
     )
 }
